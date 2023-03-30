ac49b95a5d6968440c82228a5a236ac8e7576080
FERNANDA C DE LIMA <fernanda.calixto.lima@gmail.com>
2023-03-30 16:33:13 -0400

    Produto e associacao muitos para muitos

--- src/main/java/com/fernanda/uml/repositories/ProdutoRepository.java
+++ src/main/java/com/fernanda/uml/repositories/ProdutoRepository.java
@@ -5,10 +5,10 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.fernanda.uml.domain.Categoria;
+import com.fernanda.uml.domain.Produto;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
 
 
 }
