7bd63ef15f4b3a8ca29b9b6de0c0523db0aca769
FERNANDA C DE LIMA <fernanda.calixto.lima@gmail.com>
2023-03-31 14:52:21 -0400

    Estado e Cidade

--- src/main/java/com/fernanda/uml/Uml1Application.java
+++ src/main/java/com/fernanda/uml/Uml1Application.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.fernanda.uml.domain.Categoria;
+import com.fernanda.uml.domain.Cidade;
+import com.fernanda.uml.domain.Estado;
 import com.fernanda.uml.domain.Produto;
 import com.fernanda.uml.repositories.CategoriaRepository;
+import com.fernanda.uml.repositories.CidadeRepository;
+import com.fernanda.uml.repositories.EstadoRepository;
 import com.fernanda.uml.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -19,6 +23,11 @@ public class Uml1Application implements CommandLineRunner{
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Uml1Application.class, args);
@@ -44,6 +53,20 @@ public class Uml1Application implements CommandLineRunner{
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		
 		
 		
 		}
--- src/main/java/com/fernanda/uml/repositories/CidadeRepository.java
+++ src/main/java/com/fernanda/uml/repositories/CidadeRepository.java
@@ -3,10 +3,10 @@ package com.fernanda.uml.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.fernanda.uml.domain.Categoria;
+import com.fernanda.uml.domain.Cidade;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
 
 
 }
